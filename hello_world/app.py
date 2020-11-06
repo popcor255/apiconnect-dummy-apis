0ab0626804562788f102d6cae64d05fa0ca8c6e6
popcor255 <popcor255@gmail.com>
2020-11-06 11:50:30 -0500

    demo of api-connect

--- hello_world/app.py
+++ hello_world/app.py
@@ -2,20 +2,92 @@ from flask import Flask, jsonify
 from flask import request
 from flasgger import Swagger
 import os
+import jwt
 
 app = Flask(__name__)
 swagger = Swagger(app)
 
+@app.route("/")
+def home():
+  """
+  Welcome home!
+  ---
+  responses:
+    200:
+      description: goes to home page
+  """
+  return "welcome home"
+
+
 @app.route("/helloworld")
 def helloworld():
-    """
-    This hello world api prints hello world
-    ---
-    responses:
-      200:
-        description: print hello world
-    """
-    return "hello world"
+  """
+  This hello world api prints hello world
+  ---
+  responses:
+    200:
+      description: print hello world
+  """
+  return "hello world"
+
+@app.route("/headers")
+def headers():
+  """
+  This will return the headers sent in the request
+  ---
+  responses:
+    200:
+      description: return the headers
+  """
+  return dict(request.headers)
+
+@app.route("/decodejwt", methods = ['POST'])
+def decode_jwt():
+  """
+  This will return decoded jwt [WIP]
+  ---
+  responses:
+    200:
+      description: return the headers
+  """
+  encoded = request.headers["X-Introspect-Id-Token"]
+  decoded = jwt.decode(encoded, verify=False)
+  return decoded
+
+@app.route("/patient", methods = ['POST'])
+def patient():
+  """
+  This will return reponse depending on the user role [WIP]
+  ---
+  responses:
+    200:
+      description: return the headers
+  """
+  encoded = request.headers["X-Introspect-Id-Token"]
+  decoded = jwt.decode(encoded, verify=False)
+  roles = decoded['resource_access']['api-connect']["roles"]
+  if "patient" in roles:
+    return "you are a valid user"
+
+  return "invalid user"
+  
+
+@app.route("/provider", methods = ['POST'])
+def provider():
+  """
+  This will return reponse depending on the user role [WIP]
+  ---
+  responses:
+    200:
+      description: return the headers
+  """
+  encoded = request.headers["X-Introspect-Id-Token"]
+  decoded = jwt.decode(encoded, verify=False)
+  roles = decoded['resource_access']['api-connect']["roles"]
+  if "provider" in roles:
+    return "you are a valid user"
+
+  return "invalid user"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
